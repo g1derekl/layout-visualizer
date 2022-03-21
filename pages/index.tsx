2e936e2d2a45cffb06e650e84b5d80f375ae7174
Derek Li <derek.li@alum.utoronto.ca>
2022-03-21 01:15:00 -0700

    WIP trying to put stuff on sphere

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,64 @@
+/* global JSX */
 import * as THREE from 'three';
-import React, { useRef, useState } from 'react';
+import React, {
+  ReactElement,
+  useEffect,
+  useRef
+} from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Edges } from '@react-three/drei';
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
-function Ball(props: JSX.IntrinsicElements['mesh']) {
-  const ref = useRef<THREE.Mesh>(null!)
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+function Labels(): ReactElement {
+  const elem = document.createElement('canvas');
+  const canvasRef = useRef<HTMLCanvasElement>(elem);
+  const textureRef = useRef<THREE.CanvasTexture>(null);
+
+  const write = (context: CanvasRenderingContext2D): void => {
+    context.font = '8px sans-serif';
+    context.fillStyle = '#ff5733';
+    context.fillText('PIN', 50, 60);
+    context.fillText('CG', 50, 90);
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const context = canvas.getContext('2d')!;
+      write(context);
+    }
+  }, [write]);
+
+  return (
+    <canvasTexture
+      wrapS={THREE.RepeatWrapping}
+      wrapT={THREE.RepeatWrapping}
+      repeat={new THREE.Vector2(1, 1)}
+      attach="map"
+      ref={textureRef}
+      image={canvasRef.current}
+    />
+  );
+}
+
+function Ball(props: JSX.IntrinsicElements['mesh']): ReactElement {
+  const meshRef = useRef<THREE.Mesh>(null!);
   return (
     <mesh
       {...props}
-      ref={ref}
-      scale={1}>
+      ref={meshRef}
+      scale={1}
+    >
       <sphereGeometry args={[4.25, 128, 128]} />
-      <Edges scale={1} threshold={0} color="gray" />
-      <meshBasicMaterial color="white" />
+      {/* <Edges scale={1} threshold={0} color="gray" /> */}
+      <meshStandardMaterial>
+        <Labels />
+      </meshStandardMaterial>
     </mesh>
-  )
+  );
 }
 
-export default function Home() {
+export default function Home(): ReactElement {
   return (
     <div className={styles.container}>
       <Canvas>
@@ -30,5 +67,5 @@ export default function Home() {
         <Ball position={[0, 0, -3]} />
       </Canvas>
     </div>
-  )
+  );
 }
